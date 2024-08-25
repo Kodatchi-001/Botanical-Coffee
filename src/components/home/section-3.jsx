794cf816e7d2cf6af1ffea97072cfc60300ae5f6
Ahmed Hariri <150302563+Kodatchi-001@users.noreply.github.com>
2024-08-25 21:18:27 +0100

    Create Function Filter Card Shop

--- src/components/home/section-3.jsx
+++ src/components/home/section-3.jsx
@@ -1,6 +1,7 @@
 import coffee from "../../assets/coffee.png";
 import coffee_2 from "../../assets/coffee-2.png";
-
+import coffee_3 from "../../assets/coffee-3.png";
+import { Link } from "react-router-dom";
 
 export default function Section_3() {
     return <>
@@ -10,9 +11,9 @@ export default function Section_3() {
                     Unnecessarily Good Good(s)
                 </h1>
                 <div className="w-full lg:w-4/6 xl:w-full h-full flex justify-center lg:justify-start items-end pb-8">
-                    <a href="" className="text-lg px-8 py-2 rounded-full bg-black text-white">
+                    <Link to="/Shop" className="text-lg px-8 py-2 rounded-full bg-black text-white">
                         Shop All
-                    </a>
+                    </Link>
                 </div>
             </div>
             <div className="w-full h-2/3 flex justify-between items-center flex-wrap gap-6 lg:gap-0 px-2 lg:px-0">
@@ -22,7 +23,7 @@ export default function Section_3() {
                             <img src={coffee} className="w-full h-full rounded-3xl" />
                         </div>
                         <div className="w-full h-1/3 p-4 absolute">
-                            <i class='bx bxs-heart text-xl button-blur-icone px-4 py-3 rounded-full text-white'></i>
+                            <i class='bx bxs-heart text-xl button-blur-icone px-4 py-3 rounded-full icones text-white'></i>
                         </div>
                     </div>
                     <div className="w-full h-[30%] flex flex-wrap lg:gap-2 xl:gap-0">
@@ -32,27 +33,9 @@ export default function Section_3() {
                         </div>
                         <div className="w-full h-1/2 flex justify-between items-center">
                             <h1 className="text-xl">$12 – $39</h1>
-                            <i className="bx bx-chevron-right text-5xl p-2 scale-90 rounded-full cursor-pointer bg-black text-white"></i>
-                        </div>
-                    </div>
-                </div>
-                <div className="w-full sm:w-[45%] lg:w-[31.5%] lg:h-[80%] xl:h-[95%] rounded-3xl flex flex-col justify-between p-3 gap-3 lg:gap-0 bg-white cards-shop relative">
-                    <div className="w-full h-4/6 flex relative">
-                        <div className="w-full h-full rounded-3xl overflow-hidden">
-                            <img src={coffee} className="w-full h-full rounded-3xl" />
-                        </div>
-                        <div className="w-full h-1/3 p-4 absolute">
-                            <i class='bx bxs-heart text-xl button-blur-icone px-4 py-3 rounded-full text-white'></i>
-                        </div>
-                    </div>
-                    <div className="w-full h-[30%] flex flex-wrap lg:gap-2 xl:gap-0">
-                        <div className="w-full h-1/2 flex flex-wrap">
-                            <h1 className="w-full text-3xl">Gogogu</h1>
-                            <h1 className="text-gray-500">Cherry truffle, Blueberry, Kakao nibs</h1>
-                        </div>
-                        <div className="w-full h-1/2 flex justify-between items-center">
-                            <h1 className="text-xl">$12 – $39</h1>
-                            <i className="bx bx-chevron-right text-5xl p-2 scale-90 rounded-full cursor-pointer bg-black text-white"></i>
+                            <Link to="/Shop">
+                                <i className="bx bx-chevron-right text-5xl p-2 scale-90 rounded-full cursor-pointer bg-black text-white"></i>
+                            </Link>
                         </div>
                     </div>
                 </div>
@@ -62,7 +45,7 @@ export default function Section_3() {
                             <img src={coffee_2} className="w-full h-full rounded-3xl" />
                         </div>
                         <div className="w-full h-1/3 p-4 absolute">
-                            <i class='bx bxs-heart text-xl button-blur-icone px-4 py-3 rounded-full text-white'></i>
+                            <i class='bx bxs-heart text-xl button-blur-icone px-4 py-3 rounded-full icones text-white'></i>
                         </div>
                     </div>
                     <div className="w-full h-[30%] flex flex-wrap lg:gap-2 xl:gap-0">
@@ -72,7 +55,31 @@ export default function Section_3() {
                         </div>
                         <div className="w-full h-1/2 flex justify-between items-center">
                             <h1 className="text-xl">$49</h1>
-                            <i className="bx bx-chevron-right text-5xl p-2 scale-90 rounded-full cursor-pointer bg-black text-white"></i>
+                            <Link to="/Shop">
+                                <i className="bx bx-chevron-right text-5xl p-2 scale-90 rounded-full cursor-pointer bg-black text-white"></i>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+                <div className="w-full sm:w-[45%] lg:w-[31.5%] lg:h-[80%] xl:h-[95%] rounded-3xl flex flex-col justify-between p-3 gap-3 lg:gap-0 bg-white cards-shop relative">
+                    <div className="w-full h-4/6 flex relative">
+                        <div className="w-full h-full rounded-3xl overflow-hidden">
+                            <img src={coffee_3} className="w-full h-full rounded-3xl" />
+                        </div>
+                        <div className="w-full h-1/3 p-4 absolute">
+                            <i class='bx bxs-heart text-xl button-blur-icone px-4 py-3 rounded-full icones text-white'></i>
+                        </div>
+                    </div>
+                    <div className="w-full h-[30%] flex flex-wrap lg:gap-2 xl:gap-0">
+                        <div className="w-full h-1/2 flex flex-wrap">
+                            <h1 className="w-full text-3xl">Porcelain Cup</h1>
+                            <h1 className="text-gray-500">Cherry truffle, Blueberry, Kakao nibs</h1>
+                        </div>
+                        <div className="w-full h-1/2 flex justify-between items-center">
+                            <h1 className="text-xl">$39</h1>
+                            <Link to="/Shop">
+                                <i className="bx bx-chevron-right text-5xl p-2 scale-90 rounded-full cursor-pointer bg-black text-white"></i>
+                            </Link>
                         </div>
                     </div>
                 </div>
--- src/components/shop/section-1.jsx
+++ src/components/shop/section-1.jsx
@@ -1,7 +1,58 @@
+import { useState } from "react";
 import Footer from "../footer";
 import Header_v2 from "../header-v2";
 
 export default function Shop_section_1() {
+    const [filter, setFilter] = useState("All");
+
+    const filterItems = category => setFilter(category);
+
+    const items = [
+        {
+            id: 1,
+            category: "Coffee",
+            title: "Marsha",
+            description: "Cherry truffle, Blueberry, Kakao nibs",
+            price: "$12 – $39",
+        },
+        {
+            id: 2,
+            category: "Gear",
+            title: "V60 Dripper",
+            description: "Cherry truffle, Blueberry, Kakao nibs",
+            price: "$49",
+        },
+        {
+            id: 3,
+            category: "Merchandise",
+            title: "Porcelain Cup",
+            description: "Cherry truffle, Blueberry, Kakao nibs",
+            price: "$39",
+        },
+        {
+            id: 4,
+            category: "Merchandise",
+            title: "T-shirt Green",
+            description: "Cherry truffle, Blueberry, Kakao nibs",
+            price: "$79",
+        },
+        {
+            id: 5,
+            category: "Gear",
+            title: "V60 Paper filter",
+            description: "Cherry truffle, Blueberry, Kakao nibs",
+            price: "$19",
+        },
+        {
+            id: 6,
+            category: "Gear",
+            title: "Aeropress GO",
+            description: "Cherry truffle, Blueberry, Kakao nibs",
+            price: "$19",
+        },
+
+    ];
+
     return <>
         <section className="w-full h-full flex flex-wrap mb-14">
             <div className="w-full h-[15vh] lg:h-[10vh] flex justify-end p-5">
@@ -19,114 +70,51 @@ export default function Shop_section_1() {
                 </div>
                 <div className="w-full h-full flex flex-wrap gap-5">
                     <div className="w-full h-[12vh] flex justify-center lg:justify-start items-center flex-wrap gap-2 text-xl">
-                        <button className="px-8 py-[0.6rem] rounded-full bg-black text-white">View All</button>
-                        <button className="px-8 py-[0.6rem] rounded-full border-2 border-black">Coffee</button>
-                        <button className="px-8 py-[0.6rem] rounded-full border-2 border-black">Brewing Gear</button>
-                        <button className="px-8 py-[0.6rem] rounded-full border-2 border-black">Merchandise</button>
+                        <button
+                            className={`px-8 py-[0.6rem] rounded-full ${filter == "All" ? "bg-black text-white" : "border-2 border-black"}`}
+                            onClick={() => filterItems("All")}>
+                            View All
+                        </button>
+                        <button
+                            className={`px-8 py-[0.6rem] rounded-full ${filter == "Coffee" ? "bg-black text-white" : "border-2 border-black"}`}
+                            onClick={() => filterItems("Coffee")}>
+                            Coffee
+                        </button>
+                        <button
+                            className={`px-8 py-[0.6rem] rounded-full ${filter == "Gear" ? "bg-black text-white" : "border-2 border-black"}`}
+                            onClick={() => filterItems("Gear")}>
+                            Brewing Gear
+                        </button>
+                        <button
+                            className={`px-8 py-[0.6rem] rounded-full ${filter == "Merchandise" ? "bg-black text-white" : "border-2 border-black"}`}
+                            onClick={() => filterItems("Merchandise")}>
+                            Merchandise
+                        </button>
                     </div>
-                    <div className="w-full h-full flex flex-wrap justify-between gap-5 px-3 lg:px-0">
-                        <div className="w-full sm:w-[45%] lg:w-[32%] h-[50vh] lg:h-[50vh] lg:h-[60vh] rounded-3xl flex flex-col justify-between p-3 gap-3 lg:gap-0 bg-white cards-shop">
-                            <div className="w-full h-4/6 flex rounded-3xl" id="coffee_1">
-                                <div className="w-full h-1/3 p-4">
-                                    <i class='bx bxs-heart text-xl button-blur-icone px-4 py-3 rounded-full text-white'></i>
+                    <div className="w-full h-full flex flex-wrap gap-5 px-3 lg:px-0">
+                        {items
+                            .filter(item => filter === "All" || item.category === filter)
+                            .map(item => (
+                                <div key={item.id} className="w-full sm:w-[45%] lg:w-[32%] h-[50vh] lg:h-[60vh] rounded-3xl flex flex-col justify-between p-3 gap-3 lg:gap-0 bg-white cards-shop">
+                                    <div className="w-full h-4/6 flex rounded-3xl relative">
+                                        <div className= "w-full h-full rounded-3xl" id={`background-image-cards-${item.id}`}>
+                                        </div>
+                                        <div className="w-full h-1/3 p-4 absolute">
+                                            <i className='bx bxs-heart text-xl button-blur-icone px-4 py-3 rounded-full text-white'></i>
+                                        </div>
+                                    </div>
+                                    <div className="w-full h-[30%] flex flex-wrap lg:gap-2 xl:gap-0">
+                                        <div className="w-full h-1/2 flex flex-wrap gap-2">
+                                            <h1 className="w-full text-3xl">{item.title}</h1>
+                                            <h1 className="text-gray-500">{item.description}</h1>
+                                        </div>
+                                        <div className="w-full h-1/2 flex justify-between items-center">
+                                            <h1 className="text-xl">{item.price}</h1>
+                                            <i className="bx bx-chevron-right text-5xl p-2 scale-90 rounded-full cursor-pointer bg-black text-white"></i>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="w-full h-[30%] flex flex-wrap lg:gap-2 xl:gap-0">
-                                <div className="w-full h-1/2 flex flex-wrap">
-                                    <h1 className="w-full text-3xl">Marsha</h1>
-                                    <h1 className="text-gray-500">Cherry truffle, Blueberry, Kakao nibs</h1>
-                                </div>
-                                <div className="w-full h-1/2 flex justify-between items-center">
-                                    <h1 className="text-xl">$12 – $39</h1>
-                                    <i className="bx bx-chevron-right text-5xl p-2 scale-90 rounded-full cursor-pointer bg-black text-white"></i>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="w-full sm:w-[45%] lg:w-[32%] h-[50vh] lg:h-[60vh] rounded-3xl flex flex-col justify-between p-3 gap-3 lg:gap-0 bg-white cards-shop">
-                            <div className="w-full h-4/6 flex rounded-3xl" id="coffee_2">
-                                <div className="w-full h-1/3 p-4">
-                                    <i class='bx bxs-heart text-xl button-blur-icone px-4 py-3 rounded-full text-white'></i>
-                                </div>
-                            </div>
-                            <div className="w-full h-[30%] flex flex-wrap lg:gap-2 xl:gap-0">
-                                <div className="w-full h-1/2 flex flex-wrap">
-                                    <h1 className="w-full text-3xl">Marsha</h1>
-                                    <h1 className="text-gray-500">Cherry truffle, Blueberry, Kakao nibs</h1>
-                                </div>
-                                <div className="w-full h-1/2 flex justify-between items-center">
-                                    <h1 className="text-xl">$12 – $39</h1>
-                                    <i className="bx bx-chevron-right text-5xl p-2 scale-90 rounded-full cursor-pointer bg-black text-white"></i>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="w-full sm:w-[45%] lg:w-[32%] h-[50vh] lg:h-[60vh] rounded-3xl flex flex-col justify-between p-3 gap-3 lg:gap-0 bg-white cards-shop">
-                            <div className="w-full h-4/6 flex rounded-3xl" id="coffee_3">
-                                <div className="w-full h-1/3 p-4">
-                                    <i class='bx bxs-heart text-xl button-blur-icone px-4 py-3 rounded-full text-white'></i>
-                                </div>
-                            </div>
-                            <div className="w-full h-[30%] flex flex-wrap lg:gap-2 xl:gap-0">
-                                <div className="w-full h-1/2 flex flex-wrap">
-                                    <h1 className="w-full text-3xl">Marsha</h1>
-                                    <h1 className="text-gray-500">Cherry truffle, Blueberry, Kakao nibs</h1>
-                                </div>
-                                <div className="w-full h-1/2 flex justify-between items-center">
-                                    <h1 className="text-xl">$12 – $39</h1>
-                                    <i className="bx bx-chevron-right text-5xl p-2 scale-90 rounded-full cursor-pointer bg-black text-white"></i>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="w-full sm:w-[45%] lg:w-[32%] h-[50vh] lg:h-[60vh] rounded-3xl flex flex-col justify-between p-3 gap-3 lg:gap-0 bg-white cards-shop">
-                            <div className="w-full h-4/6 flex rounded-3xl" id="coffee_4">
-                                <div className="w-full h-1/3 p-4">
-                                    <i class='bx bxs-heart text-xl button-blur-icone px-4 py-3 rounded-full text-white'></i>
-                                </div>
-                            </div>
-                            <div className="w-full h-[30%] flex flex-wrap lg:gap-2 xl:gap-0">
-                                <div className="w-full h-1/2 flex flex-wrap">
-                                    <h1 className="w-full text-3xl">Marsha</h1>
-                                    <h1 className="text-gray-500">Cherry truffle, Blueberry, Kakao nibs</h1>
-                                </div>
-                                <div className="w-full h-1/2 flex justify-between items-center">
-                                    <h1 className="text-xl">$12 – $39</h1>
-                                    <i className="bx bx-chevron-right text-5xl p-2 scale-90 rounded-full cursor-pointer bg-black text-white"></i>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="w-full sm:w-[45%] lg:w-[32%] h-[50vh] lg:h-[60vh] rounded-3xl flex flex-col justify-between p-3 gap-3 lg:gap-0 bg-white cards-shop">
-                            <div className="w-full h-4/6 flex rounded-3xl" id="coffee_5">
-                                <div className="w-full h-1/3 p-4">
-                                    <i class='bx bxs-heart text-xl button-blur-icone px-4 py-3 rounded-full text-white'></i>
-                                </div>
-                            </div>
-                            <div className="w-full h-[30%] flex flex-wrap lg:gap-2 xl:gap-0">
-                                <div className="w-full h-1/2 flex flex-wrap">
-                                    <h1 className="w-full text-3xl">Marsha</h1>
-                                    <h1 className="text-gray-500">Cherry truffle, Blueberry, Kakao nibs</h1>
-                                </div>
-                                <div className="w-full h-1/2 flex justify-between items-center">
-                                    <h1 className="text-xl">$12 – $39</h1>
-                                    <i className="bx bx-chevron-right text-5xl p-2 scale-90 rounded-full cursor-pointer bg-black text-white"></i>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="w-full sm:w-[45%] lg:w-[32%] h-[50vh] lg:h-[60vh] rounded-3xl flex flex-col justify-between p-3 gap-3 lg:gap-0 bg-white cards-shop">
-                            <div className="w-full h-4/6 flex rounded-3xl" id="coffee_6">
-                                <div className="w-full h-1/3 p-4">
-                                    <i class='bx bxs-heart text-xl button-blur-icone px-4 py-3 rounded-full text-white'></i>
-                                </div>
-                            </div>
-                            <div className="w-full h-[30%] flex flex-wrap lg:gap-2 xl:gap-0">
-                                <div className="w-full h-1/2 flex flex-wrap">
-                                    <h1 className="w-full text-3xl">Marsha</h1>
-                                    <h1 className="text-gray-500">Cherry truffle, Blueberry, Kakao nibs</h1>
-                                </div>
-                                <div className="w-full h-1/2 flex justify-between items-center">
-                                    <h1 className="text-xl">$12 – $39</h1>
-                                    <i className="bx bx-chevron-right text-5xl p-2 scale-90 rounded-full cursor-pointer bg-black text-white"></i>
-                                </div>
-                            </div>
-                        </div>
+                            ))}
                     </div>
                 </div>
             </div>
